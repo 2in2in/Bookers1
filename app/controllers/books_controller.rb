85d5d906f2b5a56aae4a37ad79d90c81a1288816
2in2in <88322988+2in2in@users.noreply.github.com>
2022-04-23 15:28:59 +0000

    [Finish]Bookers1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,50 +1,46 @@
 class BooksController < ApplicationController
-  def new
-    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
-    @book = Book.new  
-  end
-
+  
   def index
-    @book = Book.all  
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
-    @book = Book.find(params[:id])  
+  	@book = Book.find(params[:id])
+  end
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+      if @book.save
+        flash[:notice] = "successfully"
+        redirect_to book_path(@book)
+      else
+        flash.now[:error] = @book.errors.full_messages
+        render :index
+      end
   end
 
   def edit
-    @book = Book.find(params[:id])
+  	@book = Book.find(params[:id])
   end
-  
-  
-  def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to book_path(@book.id)
-    else
-      @book = Book.all
-      render :index
-    end  
-  end
-  
-  def edit
-    @book = Book.find(params[:id])
-  end
-  
+
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(list.id)  
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	flash[:notice] = "successfully"
+  	redirect_to book_path(book)
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to '/books'
+    redirect_to books_path
   end
-  
+
   private
   def book_params
-    params.require(:book).permit(:title, :body, )  
+    params.require(:book).permit(:title, :body)
   end
+
 end
